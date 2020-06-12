a3584d0ed3b2d0b7f01efd952c62e78dbc77a603
Akmyradov0604 <batya0604@gmail.com>
2020-06-12 17:06:52 +0500

    Contact us is working

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from news.models import News
+from django.core.mail import send_mail, EmailMessage
+from django.conf import settings
 
 def home(request):
 	news = News.objects.all().order_by('-id')[:5]
@@ -13,5 +15,17 @@ def register(request):
 
 
 def contact(request):
+	if request.method == 'POST':
+		message = request.POST.get('message', False)
+		mail_id = request.POST.get('email', False)
+		email = EmailMessage('Neutrality Contact Form', message, settings.EMAIL_HOST_USER, [mail_id])
+		email.content_type = 'html'
+
+		file  = request.FILES['attached']
+		email.attach(file.name, file.read(), file.content_type)
+
+		email.send()
+	
 	return render(request, 'accounts/contact.html')
+
 
--- neutrality/settings.py
+++ neutrality/settings.py
@@ -133,3 +133,10 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'batya0604@gmail.com'
+EMAIL_HOST_PASSWORD = 'Apollo2004B'
+EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
